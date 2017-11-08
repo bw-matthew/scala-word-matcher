5a539d112143cb1798ccf2f87b68f9570e09fac9
Matthew Franglen <matthew@franglen.org>
2017-11-08 20:23:51 +0000

    Filling it out more

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,21 +2,6 @@ object Main extends App {
   println("Hello, World!")
 }
 
-object Matcher {
-
-  def toPhrases(text: String)(implicit corpus: Corpus): Seq[String] =
-    toWords(text split "\\s+", 5) map { _.phrase } filter { _.isDefined } map { _.get } map { _ mkString " " }
-
-  private def toWords(words: Seq[String], window: Int)(implicit corpus: Corpus): Seq[Match] =
-    if (window <= 0)
-      Seq(Match(None, Some(words)))
-    else
-      toWords(words, window - 1)
-
-}
-
-sealed case class Match(phrase: Option[Seq[String]], remainder: Option[Seq[String]])
-
 sealed case class Corpus(phrases: Seq[String]) {
   val words = phrases map { _.split("\\s+") }
 
@@ -24,3 +9,43 @@ sealed case class Corpus(phrases: Seq[String]) {
     words.exists(_ sameElements phrase)
 
 }
+
+object Matcher {
+
+  def toPhrases(text: String)(implicit corpus: Corpus): Seq[String] =
+    toWords(text split "\\s+", 5)
+      .map { _.phrase }
+      .filter { _.isDefined }
+      .map { _.get }
+      .map { _ mkString " " }
+
+  private def toWords(words: Seq[String], window: Int)(implicit corpus: Corpus): Seq[Match] =
+    if (window <= 0)
+      Seq(Match(None, Some(words)))
+    else
+      Seq()
+}
+
+private sealed case class Accumulator(words: Seq[String]) {
+
+  def accumulate(word: String): Accumulator =
+    Accumulator(words :+ word)
+
+  def convert(window: Int)(implicit corpus: Corpus): Option[Match] =
+    if (words.length < window)
+      return None
+    else {
+      val phrase = words slice(words.length - window, words.length) // you've tested everything but this window
+
+      if (! corpus.matches(phrase))
+        return None
+
+      if (words.length > window)
+        Some(Match(Some(phrase), Some(words slice(0, words.length - window))))
+      else
+        Some(Match(Some(phrase), None))
+    }
+
+}
+
+private sealed case class Match(phrase: Option[Seq[String]], remainder: Option[Seq[String]])
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,14 +1,5 @@
 import org.scalatest._
 
-class MatcherSpec extends FlatSpec with Matchers {
-  "The Matcher" should "not match missing phrases" in {
-    implicit val corpus = Corpus(Seq("one two three", "a b c"))
-    val phrase = "the quick dog"
-
-    Matcher toPhrases phrase shouldEqual Seq()
-  }
-}
-
 class CorpusSpec extends FlatSpec with Matchers {
   "The Corpus" should "match existing phrases" in {
     val corpus = Corpus(Seq("one two three", "a b c"))
@@ -22,5 +13,14 @@ class CorpusSpec extends FlatSpec with Matchers {
     val phrase = "one two three four" split "\\s+"
 
     corpus matches phrase shouldEqual false
+  }
+}
+
+class MatcherSpec extends FlatSpec with Matchers {
+  "The Matcher" should "not match missing phrases" in {
+    implicit val corpus = Corpus(Seq("one two three", "a b c"))
+    val phrase = "the quick dog"
+
+    Matcher toPhrases phrase shouldEqual Seq()
   }
 }
