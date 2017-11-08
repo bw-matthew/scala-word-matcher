0042f7243659902d702964910ad364db567d7a65
Matthew Franglen <matthew@franglen.org>
2017-11-08 20:10:02 +0000

    Start on the matcher

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,17 +5,22 @@ object Main extends App {
 object Matcher {
 
   def toPhrases(text: String)(implicit corpus: Corpus): Seq[String] =
-    ???
+    toWords(text split "\\s+", 5) map { _.phrase } filter { _.isDefined } map { _.get } map { _ mkString " " }
 
+  private def toWords(words: Seq[String], window: Int)(implicit corpus: Corpus): Seq[Match] =
+    if (window <= 0)
+      Seq(Match(None, Some(words)))
+    else
+      toWords(words, window - 1)
 
 }
+
+sealed case class Match(phrase: Option[Seq[String]], remainder: Option[Seq[String]])
 
 sealed case class Corpus(phrases: Seq[String]) {
   val words = phrases map { _.split("\\s+") }
 
-  def matches(phrase: String): Boolean = {
-    val w = phrase split("\\s+")
-    words.exists(_ sameElements w)
-  }
+  def matches(phrase: Seq[String]): Boolean =
+    words.exists(_ sameElements phrase)
 
 }
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,16 +1,25 @@
 import org.scalatest._
 
+class MatcherSpec extends FlatSpec with Matchers {
+  "The Matcher" should "not match missing phrases" in {
+    implicit val corpus = Corpus(Seq("one two three", "a b c"))
+    val phrase = "the quick dog"
+
+    Matcher toPhrases phrase shouldEqual Seq()
+  }
+}
+
 class CorpusSpec extends FlatSpec with Matchers {
   "The Corpus" should "match existing phrases" in {
     val corpus = Corpus(Seq("one two three", "a b c"))
-    val phrase = "one two three"
+    val phrase = "one two three" split "\\s+"
 
     corpus matches phrase shouldEqual true
   }
 
   "The Corpus" should "not match missing phrases" in {
     val corpus = Corpus(Seq("one two three", "a b c"))
-    val phrase = "one two three four"
+    val phrase = "one two three four" split "\\s+"
 
     corpus matches phrase shouldEqual false
   }
